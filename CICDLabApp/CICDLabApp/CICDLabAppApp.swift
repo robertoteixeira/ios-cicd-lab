ce0e14b46a5ce19f7954a38813e9bb487561bf36
Roberto <roberto.teixeirafs@gmail.com>
2026-07-01 15:51:29 -0300

    Add demo iOS app

---
+++ CICDLabApp/CICDLabApp/CICDLabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDLabAppApp.swift
+//  CICDLabApp
+//
+//  Created by Roberto Teixeira on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CICDLabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
